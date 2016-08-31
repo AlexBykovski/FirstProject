182350576d86c4ba209bc3f85eb20fe29942ae5e
Алексвндр <aleksander@mbp-aleksvndr.zte.com.cn>
2016-09-01 00:11:57 +0300

    remove test

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -13,7 +13,6 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        var_dump($_SERVER);die;
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
